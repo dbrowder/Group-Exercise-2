2918817b0fb0bf4f432e8873b0caeed4df8aed0a
dbrowder <47614101+dbrowder@users.noreply.github.com>
2019-02-19 23:40:48 -0500

    Add files via upload

--- Update2.php
+++ Update2.php
@@ -1,33 +1,41 @@
 <?php
+// https://www.codeofaninja.com/2012/04/cakephp-2x-crud-tutorial.html
+// https://api.cakephp.org/3.7/
+    
     echo 'Name:';
     echo 'Name is required';
-    echo 'Password';
+    echo 'Password:';
     echo 'Password is required';
     
+ static $_backgroundColors = array(
+    'white' => 30
+);
+
+ static $_foregroundColors = array(
+    'gray' => 40
+);
+
 function edit() {
 	$id = $this->request->params['password'];
 	$this->User->id = $id;
 		
 	if($this->User->exists()) {
 		if($this->request->is('update') || 
-			$this->request->is('insert'))) {
+			$this->request->is('insert')) {
 			if($this->User->save($this->request->data)) {
 				$this->Session->setFlash('User made edits.');
 			}
 			else {
-				$this->Session->setFlash('Edit unsuccessful.
-				 Please try again.');
+				$this->Session->setFlash('Edit unsuccessful. Please try again');
 			}
-		else {
-			$this->request->data = $this->User->read();
-		}
-	else {
-		$this-Session->setFlash('This user does not exist.')
-	}
+            $this->request->data = $this->User->read();
+            $this->Session->setFlash('This user does not exist.');
+        }
+    }
 }
-
-public function delete() {
-	$id = $this-request->params['password'];
+                                         
+function delete() {
+	$id = $this->request->params['password'];
 	
 	if($this->request->is('find')) {
 		$this->Session->setFlash('No Delete methods allowed.');
@@ -38,11 +46,12 @@ public function delete() {
 		}
 		else {
 			if($this->User->delete($id)) {
-				$this->Session-setFlash('User deleted');
+				$this->Session->setFlash('User deleted');
 			}
 			else {
-				$this->Session-setFlash('User not deleted.');
+				$this->Session->setFlash('User not deleted.');
 			}
 		}
 	}
+}
 ?>
